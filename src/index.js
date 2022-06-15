6ca53eb33e23d6e743b308138f20d9ccecf8ce4c
adsystel <proyectAdsystel@gmail.com>
2022-06-15 15:57:52 -0500

    added auth

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {Amplify} from 'aws-amplify';
-import awsconfig from './aws-exports.js';
-Amplify.configure(awsconfig);
+import Amplify from 'aws-amplify';
+import config from './aws-exports';
+Amplify.configure(config);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
